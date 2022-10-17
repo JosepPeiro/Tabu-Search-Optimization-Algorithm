3d61ab9132f8ab6e43c0dae27c2c548873d2f9dd
JosepPeiro <j.peiroramos@gmail.com>
2022-10-17 08:13:24 +0200

    Primera Conclusion Algoritmo Acabado

--- TABU SEARCH.py
+++ TABU SEARCH.py
@@ -4,23 +4,8 @@ Date of beggining 13/10/2022
 """
 
 from random import randint as rdt
-
-
-"""Para eliminar"""
-def OrdenarDiccionario(diccionario:dict)->dict:
-    
-    dic_ord = {}
-    list_ord = sorted(diccionario.copy().values())
-    
-    for i in list_ord:
-        for k in diccionario.keys():
-            if diccionario[k] == i:
-                dic_ord[k] = diccionario[k]
-                
-    return dic_ord
-
-
-
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def CrearFichero(nom:str,
@@ -43,6 +28,18 @@ def CrearFichero(nom:str,
         g.close()
         
     return
+
+
+def ElementosUnicos(l:list) -> list:
+
+    lista_unicos = []
+
+    numeros_unicos = set(l)
+
+    for elem in numeros_unicos:
+        lista_unicos.append(elem)
+
+    return lista_unicos
 
 
 class Instancia: #Definicion del problema
@@ -129,11 +126,172 @@ class Solucion:
         
         return s
 
-class AlgoritmoTabuSearch:
+
+class TabuSearch:
     
-    def Resolver(self):
-        a = "a"
-        print(a)
+    def Resolver(self, inst:Instancia, intentos, seleccion, minimo_iter):
+        nl = 1
+        lista_ord = sorted(inst.objetos.copy(), 
+                       key = lambda x: x.valor / x.peso)
+        proporciones_ord = CrearRelacion(lista_ord)
+        #Solucion inicial de donde buscaremos mejoras
+
+        sol_buena = Constructivo(inst, proporciones_ord)
+        print(CalcularValorTotal(sol_buena))
+        itinerario_soluciones1 = []
+        itinerario_inserciones1 = []
+        nueva_mercancia1, inserciones1 = self.Mejorar(sol_buena, 
+                                              lista_ord,
+                                              inst.maleta1,
+                                              inst.maleta2, nl = nl)
+        elegir = int(len(nueva_mercancia1) * seleccion)
+        
+        cantera = []
+        inserciones_cantera = []
+        top = [CalcularValorTotal(nueva_mercancia1[y]) \
+               for y in range(elegir)]
+        indices = [y for y in range(elegir)]
+        for m in range(len(nueva_mercancia1) - elegir):
+            valor = CalcularValorTotal(nueva_mercancia1[m + elegir])
+            for v in range(len(top)):
+                if valor > top[v]:
+                    indices[v] = m + elegir
+                    
+        for g in indices:
+            cantera.append(nueva_mercancia1[g])
+            inserciones_cantera.append(inserciones1[g])
+            
+        itinerario_soluciones1 += cantera
+        itinerario_inserciones1 += inserciones_cantera
+        
+        itn = 0
+        while itn < intentos:
+            nl += 1
+            ctn = cantera.copy()
+            ins_ctn = inserciones_cantera.copy()
+            for r in range(len(cantera)):
+                if itn > minimo_iter:
+                    ins_ctn[r] = ins_ctn[r][int(len(ins_ctn[r]) * 0.3):]
+                    
+                
+                nueva_mercancia1, inserciones1 = self.Mejorar(ctn[r], 
+                                                              lista_ord,
+                                                              inst.maleta1,
+                                                              inst.maleta2, 
+                                                              indescartables = ins_ctn[r],
+                                                              nl = nl)
+            
+                elegir = int(len(nueva_mercancia1) * seleccion)
+                cantera = []
+                inserciones_cantera = []
+                top = [CalcularValorTotal(nueva_mercancia1[y]) \
+                       for y in range(elegir)]
+                indices = [y for y in range(elegir)]
+                for m in range(len(nueva_mercancia1) - 10):
+                    valor = CalcularValorTotal(nueva_mercancia1[m + 10])
+                    for v in range(len(top)):
+                        if valor > top[v]:
+                            indices[v] = m + 10
+                            
+                for g in indices:
+                    cantera.append(nueva_mercancia1[g])
+                    inserciones_cantera.append(inserciones1[g])
+                    
+                itinerario_soluciones1 += cantera
+                
+            itn += 1
+            
+        return itinerario_soluciones1
+    
+        
+    def Mejorar(self, sol:Solucion, l:list, 
+                peso_max1, peso_max2, indescartables = [], nl = 1):
+        
+        factor_decisivo = nl % 2
+        if factor_decisivo:
+            lista_elegida = sol.lista1
+        else:
+            lista_elegida = sol.lista2
+            
+        soluciones_pasadas = []
+        lista_inserciones = []
+        h = 1
+        while h <= len(lista_elegida):
+            insertados = [] + indescartables
+            eliminados = []
+            peso_restante1 = peso_max1 - sum([x.peso for x in sol.lista1])
+            peso_restante2 = peso_max2 - sum([x.peso for x in sol.lista2])
+            modificable = lista_elegida.copy()
+
+            if factor_decisivo:
+                peso_util = peso_restante1
+            else:
+                peso_util = peso_restante2
+            elegido = modificable[len(modificable) - h]            
+            
+            if elegido not in insertados:
+                modificable.pop(len(modificable) - h)
+                peso_util += elegido.peso
+                eliminados.append(elegido)
+                
+                if factor_decisivo:
+                    lista_negra = modificable + eliminados + sol.lista2
+                else:
+                    lista_negra = modificable + eliminados + sol.lista1
+
+                itera = len(l)
+
+                while itera and peso_util > 0:
+                    candidato = l[itera - 1]
+        
+                    if candidato.peso <= peso_util and \
+                        candidato not in  lista_negra:
+                        modificable.append(candidato)
+                        peso_util -= candidato.peso
+                        insertados.append(candidato)
+                    itera -= 1
+    
+                nueva_sol = Solucion()
+                if factor_decisivo:
+                    nueva_sol.lista1 = modificable
+                    nueva_sol.lista2 = sol.lista2
+                else:
+                    nueva_sol.lista1 = sol.lista1
+                    nueva_sol.lista2 = modificable
+                soluciones_pasadas.append(nueva_sol)
+                lista_inserciones.append(insertados)
+
+            h += 1
+            
+        return soluciones_pasadas, lista_inserciones
+    
+    def MostrarResultado(self, inst:Instancia, intentos, seleccion, minimo_iter):
+        
+        sol = self.Resolver(inst, intentos, seleccion, minimo_iter)
+        sol_unicas = ElementosUnicos(sol)
+        
+        graf = []
+        for s in sol_unicas:
+            graf.append(CalcularValorTotal(s))
+            
+        df = pd.DataFrame({
+                "index":list(range(len(sol_unicas))),
+                "solucion":graf
+                })
+        df.plot(x = "index", y = "solucion", kind = "line",
+                title = "Intentos:" + str(intentos) + "Seleccion:" + str(seleccion) + "Minimo_iter:" + str(minimo_iter))
+        plt.show()
+        print(graf[0], max(graf))
+        
+def CalcularValorTotal(sol:Solucion) -> int:
+    
+    valor_total = 0
+    for obj in sol.lista1:
+        valor_total += obj.valor
+    for obj in sol.lista2:
+        valor_total += obj.valor
+        
+    return valor_total
         
         
 def CrearRelacion(l:list)->dict:
@@ -152,13 +310,7 @@ def CrearMaletaConstructivo(inst, relacion, relacion_destruible,
     maleta = []
     
     i = 1
-    """
-    while peso_maximo > 0 and i < len(relacion) and \
-        len(relacion_destruible) > 0:
-    Este codigo podría ser util si se teme que todos los objetos 
-    puedan entrar en las listas, pero eso supondría hacer una comparacion 
-    más, y no nos es necesario en la mayoria de ocasiones
-    """
+
     while peso_maximo > 0 and i < len(relacion):
         id_relacion = list(relacion_destruible.keys())\
             [len(relacion_destruible) - i]
@@ -182,12 +334,10 @@ def Constructivo(inst:Instancia, relacion)-> Solucion:
     maleta1 = CrearMaletaConstructivo(inst, relacion, 
                                       relacion_ordenada_uso, 
                                       peso_maleta1)
-    print(relacion.keys())
-    #print(relacion_ordenada_uso)
     maleta2 = CrearMaletaConstructivo(inst, relacion, 
                                       relacion_ordenada_uso, 
                                       peso_maleta2)
-    #print(relacion_ordenada_uso)
+
     mlt1 = []
     mlt2 = []
     for k in maleta1:
@@ -198,7 +348,6 @@ def Constructivo(inst:Instancia, relacion)-> Solucion:
     sol = Solucion()
     sol.lista1 = mlt1
     sol.lista2 = mlt2
-    print(maleta2)
     
     return sol
 
@@ -208,16 +357,12 @@ def main():
     print(__doc__)
     
     inst = Instancia()
-    inst.LeerFichero("Objetos.txt")
+    inst.LeerFichero("datos_aleatorios_problema_optimizacion.txt")
     
-    lista_ord = sorted(inst.objetos.copy(), 
-                       key = lambda x: x.valor / x.peso)
-    proporciones_ord = CrearRelacion(lista_ord)
-    #Solucion inicial de donde buscaremos mejoras
-    #print(proporciones_ordenadas)
-    a = Constructivo(inst, proporciones_ord)
-    print(a)
+    tabu = TabuSearch()
+    tabu.MostrarResultado(inst, intentos = 1000, seleccion = 0.3, minimo_iter = 50)
     
-if __name__ == '__main__': 
+    
+if __name__ == '__main__':
     main()
 
